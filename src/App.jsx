ea05fa30534b0749a655a4bea6b719fa917fc643
rayan <desmetrayan621@gmail.com>
2025-06-03 12:23:08 +0200

    deploy4

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,35 @@
-import React, { useState } from 'react'; 
-import Search from './components/SearchBar/SearchBar'; 
-import Lyrics from './components/lyrics/Lyrics'; 
-import './App.sass'; 
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-export default function App() {
-  // États pour stocker les paroles, le chargement, l'erreur et le thème
-  const [lyrics, setLyrics] = useState('');
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
-  const [theme, setTheme] = useState('dark'); // Le thème sombre est activé par défaut
-
-  // Fonction pour récupérer les paroles via l'API
-  const fetchLyrics = async (artist, title) => {
-    setLyrics(''); // Effacer les anciennes paroles
-    setError(''); // Effacer les anciennes erreurs
-    setLoading(true);
-
-    try {
-      const response = await fetch(`https://api.lyrics.ovh/v1/${encodeURIComponent(artist)}/${encodeURIComponent(title)}`);
-      if (!response.ok) throw new Error("Paroles non trouvées. Verifie que l'orthographe soit correct"); // Si erreur, on lève une exception
-      const data = await response.json(); // Récupérer la réponse en JSON pour qu'elles ne soit plus en brute
-      setLyrics(data.lyrics); // Mettre à jour l'état avec les paroles reçues
-    } catch (err) {
-      setError(err.message || "Erreur lors de la recherche.");
-    } finally {
-      setLoading(false); 
-    }
-  };
-
-  // Fonction pour changer le thème entre clair et sombre
-  const toggleTheme = () => {
-    if (theme === 'light') {
-      setTheme('dark');
-    } else {
-      setTheme('light');
-    }
-  };
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <div className={`app-container ${theme}`}>
-      <header>
-        <h1>MY Lyrics</h1>
-        <button className="theme-toggle" onClick={toggleTheme}>
-          {theme === 'light' ? 'Mode Sombre' : 'Mode Clair'}
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
         </button>
-      </header>
-
-      <Search onSearch={fetchLyrics} />
-
-      <Lyrics lyrics={lyrics} error={error} loading={loading} />
-    </div>
-  );
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
+
+export default App
