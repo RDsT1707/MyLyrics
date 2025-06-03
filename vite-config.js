0369985f7c9d1f497a15d2fd245ec7b7799d6f07
rayan <desmetrayan621@gmail.com>
2025-06-03 11:02:08 +0200

    deploy

---
+++ vite-config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// Remplace par TON nom d'utilisateur et TON nom de repo
+export default defineConfig({
+  plugins: [react()],
+  base: '/MYLYRICS/', 
+});
